a9cd991ee04ef79519f3d35ab79a2b89634ca347
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-01-22 16:23:17 +0200

    Added DataMonitore
    Added data_monitor class

--- Rocnikovy_projekt/Code/Progect/data_monitor.py
+++ Rocnikovy_projekt/Code/Progect/data_monitor.py
@@ -1,14 +1,36 @@
 import os.path
+from time import sleep
+
+from Project.data_batch_file import DataBatchFile
+from Project.enums import FileType
 
 
 # DataMonitor class
 class DataMonitor:
-    def __init__(self, folder):
-        self.folder = folder
+    def __init__(self, data_name: str, monitored_folder: str,
+                 data_description: str, monitored_metrics, file_format: str, db_manager):
+        self.folder = monitored_folder
         self.processed_files = set()
+        self.data_name = data_name
+        self.data_description = data_description
+        self.is_monitoring = False
+        self.batch_files = []
+        self.monitored_metrics = monitored_metrics
+        self.monitor_interval = 3
+        self.file_format = self._process_file_format(file_format)
+        self.db_manager = db_manager
 
+    def _process_file_format(self, file: str):
+        if file == 'json':
+            return FileType.JSON
+        elif file == 'csv':
+            return FileType.CSV
+        elif file == 'parquet':
+            return FileType.PARQUET
+        else:
+            raise ValueError(f"Unsupported file format: {file}")
 
-    def new_files(self):
+    def _new_files(self):
         if os.path.exists(self.folder):
             all_files = [file for file in os.listdir(self.folder)]
 
@@ -16,14 +38,36 @@ class DataMonitor:
             new_files = []
             for file in all_files:
                 if not self.processed_files.__contains__(file):
-                    new_files.append(os.path.join(self.folder, file))
                     self.processed_files.add(file)
+                    file_format_part = file.rfind('.')
+                    if not self._process_file_format(file[file_format_part + 1:]) == self.file_format:
+                        continue
+
+                    file = file[:file_format_part]
+
+                    new_files.append(file + os.path.join("/", self.folder))
             return new_files
 
         else:
             return []
 
+    def start_monitoring(self):
+        print("Monitoring is started")
+        self.is_monitoring = True
 
-t = DataMonitor(r"C:\Users\Arsen\Desktop\Rocnikovy_projekt")
+        while self.is_monitoring:
+            input_files = self._new_files()
+            for file in input_files:
+                print(f"I read {file}")
 
-print(t.new_files())
+                batch_file = DataBatchFile(file, self.monitored_metrics, self.file_format, self.db_manager)
+                batch_file.compute_monitored_metrics()
+                self.batch_files.append(batch_file)
+
+        sleep(self.monitor_interval)
+
+
+
+    def stopMonitoring(self):
+        print("Monitoring is stopped")
+        self.is_monitoring = False
