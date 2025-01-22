62555829b6b6818ae663fdcb93ff45bc8d10ed56
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-01-22 16:21:56 +0200

    Batch_file added
    Added data_batch_file class

--- Rocnikovy_projekt/Code/Progect/data_batch_file.py
+++ Rocnikovy_projekt/Code/Progect/data_batch_file.py
@@ -1,26 +1,68 @@
+import datetime
+
 import pandas as pd
 
 import json
+import re
+import os
+
+from Project.database_manager import DBManager
+from Project.metric import Metric
 from enums import FileType
-from pandas import json_normalize
 
 
 class DataBatchFile:
-    def __init__(self, file_path: str, file_type: FileType):
-        self.file_path = file_path
-        self.file_type = file_type
 
-    def load_data(self):
+    def __init__(self, file: str, monitored_metrics, file_type:FileType, db_manager: DBManager):
+        self.monitored_metrics = monitored_metrics
+        self.file_type = file_type.value
+        self.name, self.time_stamp, self.path = self._parse_input(file)
+        self.read = datetime.datetime.now()
+        self.db_manager = db_manager
+
+    def _parse_input(self, input_string: str):
+        timestamp_pattern = r"\d{14}"
+        timestamp_match = re.search(timestamp_pattern, input_string)
+
+        if not timestamp_match:
+            raise ValueError("No valid timestamp found!")
+
+        timestamp_start = timestamp_match.start()
+        timestamp_end = timestamp_match.end()
+        timestamp = input_string[timestamp_start:timestamp_end]
+
+        name = input_string[:timestamp_start]
+        path = input_string[timestamp_end:]+ "\\" + input_string[:timestamp_end] + "."+ self.file_type.lower()
+        if not path:
+            raise ValueError("No valid path found!")
+
+        return name, timestamp, path
+
+    def _get_parsed_data(self):
         match self.file_type:
-            case FileType.CSV:
-                return pd.read_csv(self.file_path)
-            case FileType.JSON:
-                with open(self.file_path, 'r') as file:
+            case 'JSON':
+                with open(self.path, 'r') as file:
                     data = json.load(file)
 
-                data = json_normalize(data)
                 return data
-            case FileType.PARQUET:
-                return pd.read_parquet(self.file_path)
+            case 'CSV':
+                return pd.read_csv(self.path)
+            case 'Parquet':
+                return pd.read_parquet(self.path)
             case _:
-                raise ValueError(f"Unsupported file type: {self.file_type}")
+                raise ValueError("Unsupported file type!")
+
+#So far this is the solution to unchangeable monitored metrics.
+# It will be fixed after GUI implementation of changing of monitored metrics
+    def compute_monitored_metrics(self):
+        for metric in self.monitored_metrics:
+            if self.file_type == FileType.JSON.value and metric.name != 'NullObjectCount' and metric.name != 'EmptyObjectCount':
+                continue
+            else:
+                result = metric.calculate(self._get_parsed_data())
+            self.db_manager.save(self.name, self.file_type, result.metric_name, str(self.time_stamp), result.value)
+
+    def get_metric_value(self, metric: Metric ):
+        return self.db_manager.get_value(self.name, metric.name, self.file_type, self.time_stamp)
+
+
