80212cefae4d744ad718d2e7f87e4aba8c66998d
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-03-05 14:32:47 +0200

    Added UniqueValuesCount class

--- Rocnikovy_projekt/Code/Progect/metric.py
+++ Rocnikovy_projekt/Code/Progect/metric.py
@@ -232,4 +232,67 @@ class DefinedPathCount(ColumnMetric):
             return sum(self._count_nulls(val, column) for val in data)
         return 0
 
+class UniqueValuesCount(ColumnMetric):
+    def __init__(self):
+        super().__init__("UniqueCount")
+    def _is_unique(self, unique_values: set, value):
+        if pd.isna(value) or value == '':
+            return False
+        if unique_values.__contains__(value):
+            return False
+        unique_values.add(value)
+        return True
+
+    def calculate(self, data: DataFrame, column = None) -> MetricValue:
+        if column is None:
+            raise ValueError("Column was not chosen!")
+
+        unique_values = set()
+        return MetricValue(self.name, sum(1 if self._is_unique(unique_values, value) else 0 for value in data.loc[:,column]), datetime.now())
+
+class UniqueValuesCountJson(ColumnMetric):
+    def __init__(self):
+        super().__init__("UniqueCount")
+
+    def _is_unique(self, unique_values: set, value):
+        if unique_values.__contains__(value):
+            return False
+        unique_values.add(value)
+        return True
+
+    def calculate(self, data: Any, column= "$") -> MetricValue:
+        if column is None:
+            raise ValueError("Column was not chosen!")
+
+        if column == "$":
+            return MetricValue(self.name, 0, datetime.now())
+
+        if not self._valid_path(column):
+            raise ValueError("Wrong json-path!")
+
+        column = column[1:]
+        unique_values = set()
+        return MetricValue(self.name, self._count_nulls(data, column, unique_values), datetime.now())
+
+    def _count_nulls(self, data, column, unique_values: set):
+        if data is None or data=='':
+            return 0
+        if column == "" and self._is_unique(unique_values, data):
+            return 1
+        if isinstance(data, dict):
+            dot_position = column[1:].find(".")
+            if dot_position != -1:
+                name = column[1:dot_position + 1]
+                column = column[dot_position + 1:]
+            else:
+                name = column[1:]
+                column = ""
+
+            return sum(self._count_nulls(data[val], column, unique_values)
+                       if (val is not None and val == name) else 0 for val in data)
+
+        if isinstance(data, list):
+            return sum(self._count_nulls(val, column, unique_values) for val in data)
+        return 0
+
 
