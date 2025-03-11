add973d290fb47644fb2b9fd0d53c27797fc1d5f
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-03-12 00:02:49 +0200

    Minor fix

--- Rocnikovy_projekt/Code/Progect/metric_value.py
+++ Rocnikovy_projekt/Code/Progect/metric_value.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 class MetricValue:
-    def __init__(self, metric_name: str, value: float, timestamp: datetime):
+    def __init__(self, metric_name: str, value: int, timestamp: datetime):
         self.metric_name = metric_name
         self.value = value
         self.timestamp = timestamp
