a226d9db150109259f25de35e3a7da718dc0893b
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-03-11 14:42:28 +0200

    Fixed general metric tests input

--- Rocnikovy_projekt/Code/Progect/Test/test_metric_duplicate.py
+++ Rocnikovy_projekt/Code/Progect/Test/test_metric_duplicate.py
@@ -8,7 +8,7 @@ from Progect.metric import DuplicateRecordCount
 class TestClass(TestCase):
 
     def test_duplicate_count_CSV_case1(self):
-        #Path to Test_cases_file-> CSV_cases -> cars_37rows_3empty_4duplicate.csv
+        #Path to Test_files -> CSV_cases -> cars_37rows_3empty_4duplicate.csv
         file_path = input("Enter path to cars_37rows_3empty_4duplicate.csv: ")
 
         result = DuplicateRecordCount().calculate(data=pd.read_csv(file_path))
@@ -16,7 +16,7 @@ class TestClass(TestCase):
         self.assertEqual(result.value, 5)
 
     def test_duplicate_count_CSV_case2(self):
-        # Path to Test_cases_file-> CSV_cases -> house_price_545rows_12duplicate.csv
+        # Path to Test_files -> CSV_cases -> house_price_545rows_12duplicate.csv
         file_path = input("Enter path to house_price_545rows_12duplicate.csv: ")
 
         result = DuplicateRecordCount().calculate(data=pd.read_csv(file_path))
@@ -24,7 +24,7 @@ class TestClass(TestCase):
         self.assertEqual(result.value, 12)
 
     def test_duplicate_count_Parquet_case1(self):
-        # Path to Test_cases_file -> Parquet_cases -> cs_loadouts_12duplicate.csv
+        # Path to Test_files -> Parquet_cases -> cs_loadouts_12duplicate.csv
         file_path = input("Enter path to cs_loadouts_12duplicate.parquet: ")
 
         result = DuplicateRecordCount().calculate(data=pd.read_parquet(file_path))
@@ -32,7 +32,7 @@ class TestClass(TestCase):
         self.assertEqual(result.value, 12)
 
     def test_duplicate_count_Parquet_case2(self):
-        # Path to Test_cases_file -> Parquet_cases -> markets_8duplicate.parquet
+        # Path to Test_files -> Parquet_cases -> markets_8duplicate.parquet
         file_path = input("Enter path to markets_8duplicate.parquet: ")
 
         result = DuplicateRecordCount().calculate(data=pd.read_parquet(file_path))
@@ -41,7 +41,7 @@ class TestClass(TestCase):
 
 
     def test_duplicate_Json_case1(self):
-        # Path to Test_cases_file -> JSON_cases -> cs_weapons_12duplicate_19null.json
+        # Path to Test_files -> JSON_cases -> cs_weapons_12duplicate_19null.json
         file_path = input("Enter path to cs_weapons_12duplicate_19null.json: ")
 
         with open(file_path, 'r') as file:
@@ -53,7 +53,7 @@ class TestClass(TestCase):
         self.assertEqual(result.value, 12)
 
     def test_duplicate_Json_case2(self):
-        # Path to Test_cases_file -> JSON_cases -> courses_13duplicate_1null.json
+        # Path to Test_files -> JSON_cases -> courses_13duplicate_1null.json
         file_path = input("Enter path to courses_13duplicate_1null.json: ")
 
         with open(file_path, 'r') as file:
