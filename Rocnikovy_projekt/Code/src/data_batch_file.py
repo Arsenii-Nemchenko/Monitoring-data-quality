26fd0a12e1f7d54f2c559fd8285a07819f25daf9
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-04-23 23:31:02 +0300

    Changed data_batch_file

--- Rocnikovy_projekt/Code/src/data_batch_file.py
+++ Rocnikovy_projekt/Code/src/data_batch_file.py
@@ -11,16 +11,16 @@ from .enums import FileType
 
 
 class DataBatchFile:
-    def __init__(self, file: str, monitored_metrics, monitored_column_metrics, column, file_type:FileType, db_manager: DBManager):
+    def __init__(self, file, monitored_metrics, monitored_column_metrics, column, file_type:FileType, db_manager: DBManager):
         self.monitored_metrics = monitored_metrics
         self.monitored_column_metrics = monitored_column_metrics
         self.column = column
         self.file_type = file_type
-        self.name, self.time_stamp, self.path = self._parse_input(file)
+        self.name, self.time_stamp, self.path = self._parse_filename(file)
         self.read = datetime.datetime.now()
         self.db_manager = db_manager
 
-    def _parse_input(self, input_string: str):
+    def _parse_filename(self, input_string: str):
         timestamp_pattern = r"\d{14}"
         timestamp_match = re.search(timestamp_pattern, input_string)
 
@@ -29,14 +29,14 @@ class DataBatchFile:
 
         timestamp_start = timestamp_match.start()
         timestamp_end = timestamp_match.end()
-        timestamp = input_string[timestamp_start:timestamp_end]
+        time_stamp = input_string[timestamp_start:timestamp_end]
 
         name = input_string[:timestamp_start]
-        path = input_string[timestamp_end:]+ "\\" + input_string[:timestamp_end] + "."+ self.file_type.value.lower()
+        path = input_string[timestamp_end:] + "\\" + input_string[:timestamp_end] + "." + self.file_type.value.lower()
         if not path:
             raise ValueError("No valid path found!")
 
-        return name, timestamp, path
+        return name, time_stamp, path
 
     def _get_parsed_data(self, metric_name: str):
         match self.file_type.value:
@@ -72,5 +72,5 @@ class DataBatchFile:
                 self.db_manager.save(self.name, self.file_type.value, result.metric_name, str(self.time_stamp), result.value)
 
 
-    def get_metric_value(self, metric: Metric ):
-        return self.db_manager.get_value(self.name, metric.name, self.file_type, self.time_stamp, column=self.column)
+    def get_metric_value(self, metric_name):
+        return self.db_manager.get_value(self.name, metric_name, self.file_type, self.time_stamp, column=self.column)
