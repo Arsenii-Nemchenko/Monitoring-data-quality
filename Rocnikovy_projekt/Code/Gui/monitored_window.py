a799c6caaec775dfd8e2884bbee4ce45dbd8f119
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-05-30 20:52:40 +0300

    Monitored window update(errors)
    Errors are now handled.
    Also the user is notified about them.

--- Rocnikovy_projekt/Code/Gui/monitored_window.py
+++ Rocnikovy_projekt/Code/Gui/monitored_window.py
@@ -1,8 +1,9 @@
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import QVBoxLayout, QDialog, QHBoxLayout, QGridLayout, QLabel, QPushButton, QLineEdit, QComboBox, \
-    QSpinBox, QTextEdit, QFileDialog
+    QSpinBox, QTextEdit, QFileDialog, QToolTip
 
-from Gui.multi_selected_combobox import MultiSelectComboBox
-from src.database_manager import DBManager
+from .multi_selected_combobox import MultiSelectComboBox
+from ..src.database_manager import DBManager
 
 
 class MonitoredFileWindow(QDialog):
@@ -14,84 +15,90 @@ class MonitoredFileWindow(QDialog):
         self.selected_regular = []
         self.column = db_manager.get_column_metrics()
         self.regular = db_manager.get_regular_metrics()
-        self.setWindowTitle("New monitored file setup")
-        self.resize(700, 300)
+
+        self.setWindowTitle("New Monitored File Setup")
+        self.resize(800, 400)
 
         main_layout = QVBoxLayout()
-        base_layout = QHBoxLayout()
+        top_layout = QHBoxLayout()
         left_layout = QGridLayout()
         right_layout = QGridLayout()
 
-        #Left layout
         self.name_label = QLabel("Name:")
-        self.type_label = QLabel("File format:")
-
         self.name_text = QLineEdit()
-        self.folder_button = QPushButton("Set folder")
-        self.file_type_selector = QComboBox()
-        self.add_button = QPushButton("Add")
+        self.name_text.setPlaceholderText("Enter name*")
 
+        self.folder_button = QPushButton("Set Folder*")
+
+        self.type_label = QLabel("File Format:")
+        self.file_type_selector = QComboBox()
         file_types = db_manager.get_file_types()
         for file_type in file_types:
             self.file_type_selector.addItem(file_type)
 
         self.folder_button.clicked.connect(self.get_working_directory)
-        self.add_button.clicked.connect(self._handle_add_click)
-
-        self.name_text.setPlaceholderText("Enter name")
 
         left_layout.addWidget(self.name_label, 0, 0)
-        left_layout.addWidget(self.folder_button, 1, 0)
-        left_layout.addWidget(self.type_label, 2, 0)
-        left_layout.addWidget(self.add_button, 3, 0)
-
         left_layout.addWidget(self.name_text, 0, 1)
+        left_layout.addWidget(self.folder_button, 1, 0, 1, 2)
+        left_layout.addWidget(self.type_label, 2, 0)
         left_layout.addWidget(self.file_type_selector, 2, 1)
 
-        #Right layout
-        self.metric_type_label = QLabel("Metric type:")
-        self.time_interval_label = QLabel("Time interval (seconds):")
-        self.select_metrics_label = QLabel("Select Metrics: ")
-
-        # Metric type
+        self.metric_type_label = QLabel("Metric Type:")
         self.metric_type_selector = MultiSelectComboBox(["Regular", "Column"])
-        right_layout.addWidget(self.metric_type_label, 0, 0)
-        right_layout.addWidget(self.metric_type_selector, 0, 1)
+        self.metric_type_selector.on_selection_change = self.update_select_metrics
 
-        # Time interval
+        self.time_interval_label = QLabel("Time Interval (seconds):")
         self.time_interval_input_right = QSpinBox()
-        self.time_interval_input_right.setMinimum(1)
-        self.time_interval_input_right.setMaximum(3600)
+        self.time_interval_input_right.setRange(1, 3600)
         self.time_interval_input_right.setValue(10)
-        right_layout.addWidget(self.time_interval_label, 1, 0)
-        right_layout.addWidget(self.time_interval_input_right, 1, 1)
 
-        # Select metrics
+        self.select_metrics_label = QLabel("Select Metrics:")
         self.select_metrics = MultiSelectComboBox([])
         self.select_metrics.on_selection_change = self._handle_selected_metrics
+
+        right_layout.addWidget(self.metric_type_label, 0, 0)
+        right_layout.addWidget(self.metric_type_selector, 0, 1)
+        right_layout.addWidget(self.time_interval_label, 1, 0)
+        right_layout.addWidget(self.time_interval_input_right, 1, 1)
         right_layout.addWidget(self.select_metrics_label, 2, 0)
         right_layout.addWidget(self.select_metrics, 2, 1)
 
-        #Data description
-        layout = QHBoxLayout()
+        top_layout.addLayout(left_layout, 40)
+        top_layout.addLayout(right_layout, 60)
+
         self.description_text = QTextEdit()
         self.description_text.setPlaceholderText("Write a short description of the data here...")
-        layout.addWidget(self.description_text)
+        self.add_button = QPushButton("Add monitoring")
+        self.add_button.clicked.connect(self._handle_add_click)
 
-        self.metric_type_selector.on_selection_change = self.update_select_metrics
+        description_layout = QVBoxLayout()
+        description_layout.addWidget(self.description_text)
 
-        base_layout.addLayout(left_layout, 40)
-        base_layout.addLayout(right_layout, 60)
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.add_button, alignment=Qt.AlignLeft)
 
-        main_layout.addLayout(base_layout, 60)
-        main_layout.addLayout(layout, 40)
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(description_layout)
+        main_layout.addLayout(button_layout)
+
         self.setLayout(main_layout)
 
     def get_working_directory(self):
         self.new_directory = QFileDialog.getExistingDirectory()
 
     def _handle_add_click(self):
-        self.accept()
+        if self.new_directory:
+            if self.name_text.text().strip(" ") != "":
+                self.accept()
+            else:
+                self.name_text.setStyleSheet("border: 1px solid red;")
+                QToolTip.showText(self.name_text.mapToGlobal(QPoint(0, self.name_text.height())),
+                                  "This field cannot be empty")
+        else:
+            self.folder_button.setStyleSheet("border: 1px solid red;")
+            QToolTip.showText(self.folder_button.mapToGlobal(QPoint(0, self.folder_button.height())),
+                              "You have not chosen a directory")
 
     def _handle_selected_metrics(self):
         selected_metrics =self.select_metrics.selected_options
