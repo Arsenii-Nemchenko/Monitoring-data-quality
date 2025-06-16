867e214b59817274d100b2902b75157988c57f9a
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2025-06-16 16:35:02 +0300

    Calculation thread update

--- Rocnikovy_projekt/Code/Gui/calculation_thread.py
+++ Rocnikovy_projekt/Code/Gui/calculation_thread.py
@@ -1,59 +1,106 @@
 import threading
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QMutexLocker, QMutex
 
 
 class CalculationThread(QThread):
-    plot_ready = pyqtSignal(list, list, str)
+    plot_ready = pyqtSignal(list, list, str, str)
+    calculation_error = pyqtSignal(str)
 
-    def __init__(self, graph_widget, monitoring, metric_name, interval_sec, column):
+    def __init__(self, graph_widget, monitoring, metric_name, interval, column, is_column_metric):
         super().__init__()
         self.graph_widget = graph_widget
         self.monitoring = monitoring
         self.metric_name = metric_name
-        self.interval_sec = interval_sec
+        self.interval = interval
         self.column = column
-        self._stop_event = threading.Event()
+        self.is_column_metric = is_column_metric
+        self._stop_flag = False
+        self._connection = True
+        self._mutex = QMutex()
         self.plot_ready.connect(self.graph_widget.plot)
 
     def stop(self):
-        self._stop_event.set()
+        with QMutexLocker(self._mutex):
+            self._stop_flag = True
 
-    def _run(self):
-        print("Calculation thread started")
+        if self._connection:
+            self.plot_ready.disconnect()
+            self._connection = False
 
-        # Wait until batch_files are available
-        with self.monitoring.condition:
-            while not self.monitoring.batch_files and not self._stop_event.is_set():
-                print("Waiting for batch files")
-                self.monitoring.condition.wait(timeout=2)
+        with QMutexLocker(self.monitoring.mutex):
+            self.monitoring.data_condition.wakeAll()
+            self.monitoring.calculation_condition.wakeAll()
 
-        if self._stop_event.is_set():
-            return
+    def run(self):
+        try:
 
-        self.perform_calculation()
+            initial_data_ready = False
+            with QMutexLocker(self.monitoring.mutex):
 
-        while not self._stop_event.is_set():
-            self._stop_event.wait(self.interval_sec)
-            if self._stop_event.is_set():
-                break
-            self.perform_calculation()
+                initial_data_ready = self.monitoring.is_initial_data_ready()
+                if self._stop_flag:
+                    return
+
+            # Wait for initial data
+            while not initial_data_ready and not self._stop_flag:
+                with QMutexLocker(self.monitoring.mutex):
+
+                    if self.monitoring.is_initial_data_ready():
+                        break
+
+                    self.monitoring.data_condition.wait(self.monitoring.mutex)
+                    initial_data_ready = self.monitoring.is_initial_data_ready()
+
+            if self._stop_flag:
+                return
+
+
+            # Main processing loop
+            while not self._stop_flag:
+                self.monitoring.signal_calculation_start()
+                self.perform_calculation()
+                with QMutexLocker(self.monitoring.mutex):
+                    self.monitoring.signal_calculation_end()
+
+                with QMutexLocker(self.monitoring.mutex):
+                    if self._stop_flag:
+                        break
+                    self.monitoring.data_condition.wait(self.monitoring.mutex, (self.interval * 1000))
+
+        except Exception as e:
+            self.calculation_error.emit(f"Calculation thread error: {str(e)}")
+            self.monitoring.signal_calculation_end()
+        finally:
+            self.deleteLater()
 
     def perform_calculation(self):
         try:
-            with self.monitoring.lock:
-                data = [bf.get_metric_value(self.metric_name) for bf in self.monitoring.batch_files[self.column]]
-            if not data:
-                raise AttributeError(f"No data available for {self.metric_name} metric!")
+            with QMutexLocker(self.monitoring.mutex):
+                batch_files = self.monitoring.get_batch_files(self.column)
+                if not batch_files:
+                    raise ValueError(f"No batch files available for column {self.column}")
+
+                data = [bf.get_metric_value(self.metric_name) for bf in batch_files]
+
+            if len(data) == 0:
+                return
 
             data = [item for sublist in data for item in sublist]
             if data:
                 values, timestamps = zip(*data)
-                self.plot_ready.emit(list(timestamps), list(values), f"{self.metric_name} over time")
+                if self.is_column_metric:
+                    self.plot_ready.emit(
+                        list(timestamps),
+                        list(values),
+                        f"{self.metric_name} over time",
+                        self.column
+                    )
+                else:
+                    self.plot_ready.emit(
+                        list(timestamps),
+                        list(values),
+                        f"{self.metric_name} over time",
+                        None
+                    )
         except Exception as e:
-            print(f"Calculation error for metric '{self.metric_name}': {e}")
-
-    def run(self):
-        try:
-            self._run()
-        except Exception as e:
-            print(f"Calculation thread error: {e}")
+            print(f"I just crashed {str(e)}")
